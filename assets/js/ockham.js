306cf8a4a7e4cc967e7ccb63869ee1d6ec439ede
IsakJones <isak.jones.98@gmail.com>
2022-01-26 22:10:17 +0100

    jeff helped a lot with flex

--- assets/js/ockham.js
+++ assets/js/ockham.js
@@ -3,7 +3,7 @@
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
 
-  document.addEventListener('click', function(e) {
+  document.addEventListener('click', (e) => {
     var target = e.target;
 
     if(!checkbox.checked ||
